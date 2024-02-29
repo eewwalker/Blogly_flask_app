d21518685b7b335316a1fffd3bcf250b14ff9640
Emily Walker <ewalker415@gmail.com>
2024-02-28 17:12:47 -0800

    edit/delete user

--- app.py
+++ app.py
@@ -44,25 +44,50 @@ def handle_user():
     last_name = request.form['last_name']
     image_url = request.form['image_url']
 
-    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    new_user = User(first_name=first_name,
+                    last_name=last_name, image_url=image_url)
 
     db.session.add(new_user)
     db.session.commit()
 
     return redirect('/users')
 
-# @app.get("/users/<int:user_id>")
-#     # show info about given user
-#     # button here to edit and delete
 
-# @app.get("/users/<int:user_id>/edit")
+@app.get("/users/<int:user_id>")
+def show_user(user_id):
+    user = User.query.get(user_id)
+    return render_template('user_details.html',
+                           user=user)
+
+
+@app.get("/users/<int:user_id>/edit")
+def show_edit(user_id):
+    user = User.query.get(user_id)
+    return render_template('edit_user.html',
+                           user=user)
 #     # edit page for user
 #     # have cancel button --> redirects to /users/<int:user_id>
 #     # save button --> updates user (update db, make sure commit)
 
-# @app.post("/users/<int:user_id>/edit")
-#     # process edit form
-#     # redirect to /users
 
-# @app.post("/users/<int:user_id>/delete")
-    # delete the user
+@app.post("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    first_name = request.form["first_name"]
+    last_name = request.form['last_name']
+    image_url = request.form['image_url']
+
+    user = User.query.get(user_id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.commit()
+    return redirect('/users')
+
+
+@app.post("/users/<int:user_id>/delete")
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
